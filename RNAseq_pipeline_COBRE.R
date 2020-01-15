b288e2db176b62888c5d8128a322a67e44e86272
yuewangpanda <wangyuewy729@gmail.com>
2020-01-15 15:50:38 -0500

    V1.1
    Full length paired-end RNA-seq pipeline version 1.1

--- RNAseq_pipeline_COBRE.R
+++ RNAseq_pipeline_COBRE.R
@@ -13,8 +13,11 @@ myAli <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/rnaseq_pipeline/frank/alignmen
 myExp <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/rnaseq_pipeline/frank/rawcounts/"
 myTmp <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/rnaseq_pipeline/frank/tmp/"
 #--
-givenInd <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/genomic_references/mouse/STAR/GRCm38_index"
-givenGen <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/genomic_references/mouse/ensembl-annotations/Mus_musculus.GRCm38.97.gtf"
+StarInd <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/genomic_references/mouse/STAR/GRCm38_index"
+StarRef <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/genomic_references/mouse/ensembl-annotations/Mus_musculus.GRCm38.97.gtf"
+PicardRef <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/genomic_references/mouse/CollectRnaSeqMetrics/Mus_musculus.GRCm38.97.refFlat.txt"
+PicardInt <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/genomic_references/mouse/CollectRnaSeqMetrics/Mus_musculus.GRCm38.97.rRNA.interval_list"
+RsemRef <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/genomic_references/mouse/RSEM/txdir_GRCm38/RSEMref"
 #--
 mytar <- sampleID
 
@@ -37,6 +40,7 @@ for(i in 1:length(mytar)){
 	#-----------------
 	# Quality control
 	#-----------------
+	writeLines("echo 'QC step'", conOut)
 	comm <- "/isi/whitfield/ywang/SofWar/FastQC/fastqc myFASTQ --outdir="
 	tmp <- gsub("myFASTQ", glue(myRaw, my_R1), comm)
 	tmp <- glue(tmp, myQC)
@@ -47,12 +51,13 @@ for(i in 1:length(mytar)){
 	tmp <- glue(tmp, myQC)
 	writeLines(tmp, conOut)
 	writeLines("\n", conOut)
-	#-------
-	# Trime
-	#-------
+	#------
+	# Trim
+	#------
+	writeLines("echo 'Trim step'", conOut)
 	comm_1 <- "cutadapt -A 'A{76}' -g 'T{76}'"
 	comm_2 <- "-o R1_fastq_out -p R2_fastq_out R1_fastq_in R2_fastq_in"
-	comm_3 <- "-m 5 --nextseq-trim=20 > Sample_ID_L000.report"  #-j 8 need python 3
+	comm_3 <- "-m 5 --nextseq-trim=20 > Sample_ID.report"  #-j 8 need python 3
 
 	my_R1_in <- paste(myRaw, sample_id, "_R1_001.fastq.gz", sep = "")
 	my_R2_in <- paste(myRaw, sample_id, "_R2_001.fastq.gz", sep = "")
@@ -75,18 +80,17 @@ for(i in 1:length(mytar)){
 	#----------
 	# Alignment
 	#----------
-	myInd <- givenInd
-	myGen <- givenGen
+	writeLines("echo 'Alignment step'", conOut)
 
 	comm_1 <- "/isi/whitfield/ywang/SofWar/STAR-2.7.3a/source/STAR --quantMode TranscriptomeSAM --genomeDir myind --sjdbGTFfile mygene"
 	comm_2 <- "--runThreadN 4 --twopassMode Basic --outSAMunmapped Within --outFilterType BySJout --outSAMattributes NH HI AS NM MD --outSAMtype BAM SortedByCoordinate --outFilterMultimapNmax 20 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1"
 	comm_3 <- "--readFilesIn trimed_R1_fastq trimed_R2_fastq --readFilesCommand zcat --outFileNamePrefix Sample_ID."
 
-	comm_1 <- gsub("myind", myInd, comm_1)
-	comm_1 <- gsub("mygene", myGen, comm_1)
+	comm_1 <- gsub("myind", StarInd, comm_1)
+	comm_1 <- gsub("mygene", StarRef, comm_1)
 
-	myTrim_R1 <- paste(myTri, sample_id, "_L000_R1.fastq.gz", sep = "")
-	myTrim_R2 <- paste(myTri, sample_id, "_L000_R2.fastq.gz", sep = "")
+	myTrim_R1 <- paste(myTri, sample_id, "_R1_001.fastq.gz", sep = "")
+	myTrim_R2 <- paste(myTri, sample_id, "_R2_001.fastq.gz", sep = "")
 	tag <- paste(myAli, sample_id, sep = "")
 
 	comm_3 <- gsub("trimed_R1_fastq", myTrim_R1, comm_3)
@@ -100,37 +104,39 @@ for(i in 1:length(mytar)){
 	#------------------
 	# QC for BAM file
 	#------------------
-	comm <- "java -Xmx32G -jar /isi/whitfield/ywang/SofWar/picard.jar CollectRnaSeqMetrics I=myInBam O=myOutBam REF_FLAT=myTxt STRAND=SECOND_READ_TRANSCRIPTION_STRAND RIBOSOMAL_INTERVALS=interval_list MAX_RECORDS_IN_RAM=1000000"
-	myBam_in <- ""
-	myBam_out <- ""
-	myFlat <- ""
+	writeLines("echo 'Alignment QC step'", conOut)
+	comm <- "java -Xmx32G -jar /isi/whitfield/ywang/SofWar/picard.jar CollectRnaSeqMetrics I=myInBam O=myOut REF_FLAT=myTxt STRAND=SECOND_READ_TRANSCRIPTION_STRAND RIBOSOMAL_INTERVALS=interval_list MAX_RECORDS_IN_RAM=1000000"
+	myBam_in <- paste(myAli, sample_id, ".Aligned.sortedByCoord.out.bam", sep = "")
+	my_out <- paste(myAli, sample_id, ".RNA_Metrics", sep = "")
 	comm <- gsub("myInBam", myBam_in, comm)
-	comm <- gsub("myOutBam", myBam_out, comm)
-	comm <- gsub("myTxt", myFlat, comm)
-	#tmp <- comm
-	#writeLines(tmp, conOut)
-	#writeLines("\n", conOut)
+	comm <- gsub("myOut", my_out, comm)
+	comm <- gsub("myTxt", PicardRef, comm)
+	comm <- gsub("interval_list", PicardInt, comm)
+	tmp <- comm
+	writeLines(tmp, conOut)
+	writeLines("\n", conOut)
 	#--
 	comm <- "java -Xmx32G -jar /isi/whitfield/ywang/SofWar/picard.jar MarkDuplicates I=myInBam O=myOutBam M=myTxt OPTICAL_DUPLICATE_PIXEL_DISTANCE=100 CREATE_INDEX=false"
-	myBam_in <- ""
-	myBam_out <- ""
-	myFlat <- ""
+	myBam_in <- paste(myAli, sample_id, ".Aligned.sortedByCoord.out.bam", sep = "")
+	myBam_out <- paste(myAli, sample_id, ".marked_duplicates.bam", sep = "")
+	mytxt <- paste(myAli, sample_id, ".marked_dup_metrics.txt", sep = "")
 	comm <- gsub("myInBam", myBam_in, comm)
 	comm <- gsub("myOutBam", myBam_out, comm)
-	comm <- gsub("myTxt", myFlat, comm)
-	#tmp <- comm
-	#writeLines(tmp, conOut)
-	#writeLines("\n", conOut)
+	comm <- gsub("myTxt", mytxt, comm)
+	tmp <- comm
+	writeLines(tmp, conOut)
+	writeLines("\n", conOut)
 	#-----------
 	# Expression
 	#-----------
+	writeLines("echo 'RSEM step'", conOut)
 	myIn <- paste(myAli, sample_id, ".Aligned.toTranscriptome.out.bam", sep = "")
 	tag <- paste(myExp, sample_id, sep = "")
 
-	comm <- "rsem-calculate-expression --paired-end --alignments --strandedness reverse -p 4 input_bam mygene Sample_ID"
+	comm <- "/isi/whitfield/ywang/SofWar/RSEM-1.3.1/rsem-calculate-expression --paired-end --alignments --strandedness reverse -p 8 input_bam mygene Sample_ID"
 
 	comm <- gsub("input_bam", myIn, comm)
-	comm <- gsub("mygene", myGen, comm)
+	comm <- gsub("mygene", RsemRef, comm)
 	comm <- gsub("Sample_ID", tag, comm)
 	
 	tmp <- comm
@@ -140,7 +146,7 @@ for(i in 1:length(mytar)){
 
 mysub <- paste(myTmp, "submit.sp", sep="")
 conOut <- file(mysub, "w")
-curLine <- paste("qsub -A=NCCC -l nodes=1:ppn=10 -l walltime=20:0:0 ", whoseData, "_", mytar, ".sp", sep = "")
+curLine <- paste("mksub -l nodes=1:ppn=20 -l walltime=20:0:0 ", whoseData, "_", mytar, ".sp", sep = "")
 writeLines(curLine, conOut)
 close(conOut)
 
@@ -152,4 +158,33 @@ system(comm)
 ###################
 # [2] Multiqc for every step #
 ###################
-"https://github.com/ewels/MultiQC/blob/master/docs/usage.md"
+
+rm(list = ls())
+
+whoseData <- "Meisam_PattabiramanLab"
+myQC <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/rnaseq_pipeline/frank/fastqc/"
+myTri <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/rnaseq_pipeline/frank/trim/"
+myAli <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/rnaseq_pipeline/frank/alignment/"
+myExp <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/rnaseq_pipeline/frank/rawcounts/"
+myTmp <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/rnaseq_pipeline/frank/tmp/"
+myMul <- "/dartfs-hpc/rc/lab/G/GMBSR_bioinfo/misc/rnaseq_pipeline/frank/multiqc/"
+myoutf <- paste(myTmp, whoseData, "_MultiQC.sp", sep = "")
+comm <- "multiqc fastqc trim bam -n myfolder"
+
+conOut <- file(myoutf, "w")
+tmp <- "#!/bin/bash"
+writeLines(tmp, conOut)
+writeLines("\n", conOut)
+tmp <- "source activate /isi/whitfield/ywang/SofWar/QCpipe"
+writeLines(tmp, conOut)
+writeLines("\n", conOut)
+tmp <- gsub("fastqc", myQC, comm)
+tmp <- gsub("trim", myTri, tmp)
+tmp <- gsub("bam", myAli, tmp)
+tmp <- gsub("myfolder", paste(myMul, whoseData, "_multiqc_report.html", sep = ""), tmp)
+writeLines(tmp, conOut)
+writeLines("\n", conOut)
+tmp <- "source deactivate"
+writeLines(tmp, conOut)
+writeLines("\n", conOut)
+close(conOut)
