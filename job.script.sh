28fb72092f47f02698a0fc3c6ba264a20fe22cb0
Mike Martinez <mike.j.martinez99@gmail.com>
2025-04-15 09:08:38 -0400

    Cleaned job script

--- job.script.sh
+++ job.script.sh
@@ -2,27 +2,19 @@
 
 # Name of the job
 #SBATCH --job-name=RNAseq.preprocess
-
-# Number of compute nodes
 #SBATCH --nodes=1
-
-# specify que to submit to (preempt1 only contains q10 DAC node)
 #SBATCH --partition=standard
-
-# specify account you are submitting from
-#SBATCH --account=nccc
-
-# Walltime (job duration)
 #SBATCH --time=60:00:00
-
-# Email address
 #SBATCH --mail-user=XXXXXXXXX@dartmouth.edu
-
-# Email notifications (comma-separated options: BEGIN,END,FAIL)
 #SBATCH --mail-type=FAIL
+#SBATCH --output=RNAseq.preprocess_%j.out
 
 source /optnfs/common/miniconda3/etc/profile.d/conda.sh
 conda activate /dartfs/rc/nosnapshots/G/GMBSR_refs/envs/snakemake
-snakemake -s Snakefile  --conda-frontend conda --use-conda --conda-prefix /dartfs/rc/nosnapshots/G/GMBSR_refs/envs/DAC-RNAseq-pipeline \
+snakemake -s Snakefile  \
+	--conda-frontend conda \
+	--use-conda \
+	--conda-prefix /dartfs/rc/nosnapshots/G/GMBSR_refs/envs/DAC-RNAseq-pipeline \
 	--profile cluster_profile \
-	--rerun-incomplete --keep-going
+	--rerun-incomplete \
+	--keep-going
